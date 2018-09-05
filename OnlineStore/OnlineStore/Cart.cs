6ac039be2b6a2b994189c3a6f4541da6333a2875
Utkarsh Dhawan <udhawan@tavisca.com>
2018-09-05 13:28:19 +0530

    Added shopping cart assignment

---
+++ OnlineStore/OnlineStore/Cart.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore
+{
+    public class Cart
+    {
+
+        public List<Items> AddToCart(List<Items> listItems, Items items)
+        {
+            //Adding item to cart
+            listItems.Add(items);
+            Console.WriteLine("Item added successfully");
+            return listItems;
+        }
+        public List<Items> RemoveFromCart(int id, List<Items> listItems, Items items)
+        {
+            //removing item from cart
+            for (int check = 0; check < listItems.Count; check++)
+            {
+                if (listItems[check].id == id)
+                {
+                    listItems.Remove(listItems[check]);
+                    Console.WriteLine("Item removed from cart successfully");
+                }
+            }
+            return listItems;
+        }
+        public void showItems(Items items, List<Items> listItems)
+        {
+            //Show items in a cart
+            if (listItems.Count > 0)
+            {
+                Console.WriteLine("Items in cart are:");
+                for (int index = 0; index < listItems.Count; index++)
+                {
+                    Console.WriteLine("ID: " + listItems[index].id);
+                    Console.WriteLine("Name: " + listItems[index].name);
+                    Console.WriteLine("Price: " + listItems[index].price);
+                    Console.WriteLine("Quantity: " + listItems[index].quantity);
+                    Console.WriteLine("---------------------------------------");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Cart is Empty");
+            }
+        }
+        public List<Items> clearCart(List<Items> listItems)
+        {
+            //clear cart
+            listItems.Clear();
+            return listItems;
+        }
+        public int TotalPriceInCart(List<Items> listItems)
+        {
+            int sum = 0;
+            for (int index = 0; index < listItems.Count; index++)
+            {
+                sum += ((listItems[index].price) * (listItems[index].quantity));
+            }
+            return sum;
+    }
+}
---
+++ OnlineStore/OnlineStore/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            List<Items> listItems = new List<Items>();
+            Items items = new Items();
+            Cart obj = new Cart();
+            while (true)
+            {
+                Console.WriteLine("Enter option:");
+                Console.WriteLine("1--Add to Cart");
+                Console.WriteLine("2--Remove from Cart");
+                Console.WriteLine("3--Display Cart");
+                Console.WriteLine("4--Clear cart");
+                Console.WriteLine("5--Exit");
+                int value = Convert.ToInt32(Console.ReadLine());
+                if (value == 1)
+                {
+                    Console.WriteLine("Enter ID:");
+                    items.id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Name:");
+                    items.name = Console.ReadLine();
+                    Console.WriteLine("Enter Price:");
+                    items.price = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Quantity:");
+                    items.quantity = Convert.ToInt32(Console.ReadLine());
+                     listItems=obj.AddToCart(listItems, items);
+                }
+                else if (value == 2)
+                {
+                    Console.WriteLine("Enter ID:");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    listItems=obj.RemoveFromCart(id, listItems, items);
+                }
+                else if (value == 3)
+                {
+                    obj.showItems(items, listItems);
+                }
+                else if (value == 4)
+                {
+                    listItems=obj.clearCart(listItems);
+                }
+                else if (value == 5)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Choice");                }
+            }
+        }
+    }
+}
